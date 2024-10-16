b0c8af867edc6b7340e4b33e0fdd6563a1ceccc8
Dirakon <609700zxc@mail.ru>
2024-10-16 23:44:01 +0300

    Add actual cups integration

--- printer_bot/__main__.py
+++ printer_bot/__main__.py
@@ -1,27 +1,16 @@
-from os import read
 from typing import Tuple
 from httpx import Response
-from mmpy_bot import Bot, Settings, ExamplePlugin, WebHookExample
-import sys
+from mmpy_bot import Bot, Settings
 import argparse
 from pathlib import Path
-import asyncio
 import re
-from datetime import datetime
 from pathlib import Path
-
-import click
-import mattermostautodriver
-
 from mmpy_bot.function import listen_to
 from mmpy_bot.plugins.base import List, Plugin
-from mmpy_bot.scheduler import schedule
 from mmpy_bot.wrappers import Message
-from aiohttp.client import ClientSession
 import shutil
 import unicodedata
-
-import os
+import subprocess
 
 def slugify(value, allow_unicode=False):
     """
@@ -42,7 +31,7 @@ def slugify(value, allow_unicode=False):
 
 class MyPlugin(Plugin):
     @listen_to('.*', re.IGNORECASE)
-    async def hi(self, message: Message):
+    async def process_any_message(self, message: Message):
         try:
             imgs = await self.try_get_images(message)
         except Exception as e:
@@ -67,7 +56,10 @@ class MyPlugin(Plugin):
                 self.driver.reply_to(message, f'Printed {file_name}!')
 
     def try_print(self, file_path: Path) -> None:
-        pass
+        lp_bin_path = shutil.which('lp')
+        if lp_bin_path is None:
+            raise Exception('`lp` binary not found! Is cups installed?')
+        subprocess.run([lp_bin_path, str(file_path.resolve())])
 
     async def try_get_images(self, message: Message) -> List[Tuple[Path, str]] | None:
         if not 'file_ids' in message.body['data']['post']:
@@ -79,14 +71,13 @@ class MyPlugin(Plugin):
              message_info['name'])
             for message_info in message.body['data']['post']['metadata']['files']]
 
-        for file_path, file_id, _ in file_infos:
+        for file_path, file_id, file_name in file_infos:
             if file_path.exists():
                 print(f'file with id {file_id} already downloaded, skipping')
                 continue
             q: Response  = self.driver.files.get_file(file_id)
             if q.status_code != 200:
-                print("TODO: error handling")
-                continue
+                raise Exception(f'Non 200 status_code when trying to get image {file_name}! {q.text}')
             file_path.parent.mkdir(parents=True, exist_ok=True)
             with open(file_path, 'wb') as f:
                     print(f'Saving {file_id} to {file_path}')
@@ -110,7 +101,7 @@ if __name__ == "__main__":
     bot = Bot(
         settings=Settings(
             MATTERMOST_URL = args.mattermost_url,
-            MATTERMOST_PORT = 443,
+            MATTERMOST_PORT = 443, # TODO: parametrize?
             BOT_TOKEN = Path(args.mattermost_token_file).read_text().split()[0],
             BOT_TEAM = args.mattermost_team,
             SSL_VERIFY = True,
